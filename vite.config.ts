d0c1b66aaf54f52fac8da6ad940cec9caa9ed93f
nabab-sharif <mdnababsharif2@gmail.com>
2026-09-12 09:57:30 +0600

    update some

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -13,14 +12,13 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: null,
       devOptions: { enabled: false },
       includeAssets: ['favicon.ico', 'robots.txt', 'pictures/**/*.png'],
       manifest: {
-        name: 'MNR IT Management System',
+        name: 'MNR IT',
         short_name: 'MNR IT',
         description: 'Professional IT Management System for MNR Group',
         theme_color: '#0ea5e9',
